1bca69f9aa32bcea69717ae299d52c7a588ce50f
Hunter Betz <hhb311@gmail.com>
2019-02-26 20:23:32 -0500

    Working on actions and action creators

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 // import {SIGN_IN, SIGN_OUT, ADD_CATEGORY, SELECT_CATEGORY} from './types'
 import * as actionTypes from './types';
+import api from '../api/api';
 
 /**
  * The sign in action creator. Handles the user sign in action
@@ -22,11 +23,19 @@ export const signOut = () => {
 };
 
 /**
+ * Getting categories
+ */
+export const fetchCategories = () => async dispatch => {
+    const response = await api.get('/categories');
+    dispatch({type: 'FETCH_CATEGORIES', payload: response});
+};
+
+/**
  * Category select action creator. Handles the user category select
  * action
  * @param {Handles the user selected category} category 
  */
-export const onCategorySelect = (category) => {
+export const onCategorySelect = category => {
     return {
         type: actionTypes.SELECT_CATEGORY,
         payload: category
@@ -38,7 +47,7 @@ export const onCategorySelect = (category) => {
  * a new category
  * @param {Handles user input for submitting a new category} event 
  */
-export const onCategoryFormSubmit = (category) => {
+export const onCategoryFormSubmit = category => {
     return {
         type: actionTypes.ADD_CATEGORY,
         payload: category
@@ -50,7 +59,7 @@ export const onCategoryFormSubmit = (category) => {
  * adding a new expense
  * @param {Handles user submit expense action} expense 
  */
-export const onExpenseFormSubmit = (expense) => {
+export const onExpenseFormSubmit = expense => {
     return {
         type: actionTypes.ADD_EXPENSE,
         payload: expense
