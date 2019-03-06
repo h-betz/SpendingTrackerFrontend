e304c1d6f3a0067fd18f8488ff3b18cacd011108
Hunter Betz <hhb311@gmail.com>
2019-03-06 06:57:49 -0500

    Reducer bug fix

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { reducer as formReducer } from 'redux-form';
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
+import categoryReducer from './categoryReducer';
+import expenseReducer from './expenseReducer';
 
 export default combineReducers ({
     auth: authReducer,
-    form: formReducer
+    form: formReducer,
+    category: categoryReducer,
+    expenses: expenseReducer,
 });
